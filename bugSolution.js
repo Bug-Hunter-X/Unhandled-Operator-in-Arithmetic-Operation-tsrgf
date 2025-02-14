f7f2cb19b3c39d705da7866bc3378360b8bfdf33
BugHunter-X <askzenai@gmail.com>
2025-02-14 19:26:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b; 
+}
+
+function multiply(a, b) {
+  return a * b; 
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero");
+  }
+  return a / b;
+}
+
+function modulo(a, b) {
+  if (b === 0) {
+    throw new Error("Modulo by zero");
+  }
+  return a % b;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+': return add(a, b);
+    case '-': return subtract(a, b);
+    case '*': return multiply(a, b);
+    case '/': return divide(a, b);
+    case '%': return modulo(a, b);
+    default: throw new Error('Invalid operator');
+  }
+}
+
+console.log(operate('+', 5, 3)); // 8
+console.log(operate('-', 10, 4)); // 6
+console.log(operate('*', 7, 2)); // 14
+console.log(operate('/', 15, 3)); // 5
+console.log(operate('%', 10, 3)); // 1
+console.log(operate('%', 10, 0)); // throws error
